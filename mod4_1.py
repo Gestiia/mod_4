0e4534222e1e9e4f33fba1d496829fb6f7c42637
Екатерина <marusoht648@gmail.com>
2024-11-04 16:08:53 +0300

    Add files via upload

---
+++ mod4_1.py
@@ -0,0 +1,10 @@
+import feke_math
+import true_math
+result1 = feke_math.divide(69, 3)
+result2 = feke_math.divide(3, 0)
+result3 = true_math.divide(49, 7)
+result4 = true_math.divide(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
